c56f7d57568d1cfb3d5aceef66a48f392f448087
emonShimoul <emonshimoul@gmail.com>
2025-05-27 11:03:09 +0600

    play with string

---
+++ Module 10 (String)/string_input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char s[10];
+    char s1[50] = "Shimoul Uddin Emon";
+    // scanf("%s", &s);
+    // fgets(s,20,stdin);
+    // printf("%c", s[3]);
+    printf("%s", s1);
+
+    return 0;
+}
