134cf940c1ef53c7ad80b3f6278f3e57b0f98e25
emonShimoul <emonshimoul@gmail.com>
2025-05-30 21:11:35 +0600

    string compare

--- Module 10 (String)/string_compare.c
+++ Module 10 (String)/string_compare.c
@@ -1,41 +1,52 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<string.h>
 int main()
 {
     char a[101], b[101];
     scanf("%s %s", &a, &b);
 
-    int i = 0;
-    while(true)
-    {
-        if(a[i]=='\0' && b[i]=='\0')
-        {
-            printf("Equal");
-            break;
-        }
-        else if(a[i] =='\0')
-        {
-            printf("A is smaller");
-            break;
-        }
-        else if(b[i] =='\0')
-        {
-            printf("B is smaller");
-            break;
-        }
-        else if(a[i] < b[i])
-        {
-            printf("A is smaller");
-            break;
-        }
-        else if(a[i] > b[i])
-        {
-            printf("B is smaller");
-            break;
-        }
-        else if(a[i] == b[i]){
-            i++;
-        }
+    // int i = 0;
+    // while(true)
+    // {
+    //     if(a[i]=='\0' && b[i]=='\0')
+    //     {
+    //         printf("Equal");
+    //         break;
+    //     }
+    //     else if(a[i] =='\0')
+    //     {
+    //         printf("A is smaller");
+    //         break;
+    //     }
+    //     else if(b[i] =='\0')
+    //     {
+    //         printf("B is smaller");
+    //         break;
+    //     }
+    //     else if(a[i] < b[i])
+    //     {
+    //         printf("A is smaller");
+    //         break;
+    //     }
+    //     else if(a[i] > b[i])
+    //     {
+    //         printf("B is smaller");
+    //         break;
+    //     }
+    //     else if(a[i] == b[i]){
+    //         i++;
+    //     }
+    // }
+    
+    int val = strcmp(a,b);
+    if(val<0){
+        printf("A is smaller");
+    }else if(val>0){
+        printf("B is smaller");
+    }if(val==0){
+        printf("Equal");
     }
+
     return 0;
 }
