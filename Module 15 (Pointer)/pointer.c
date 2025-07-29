f5022e03e48d7213d1bdeef9c9135cefc458554b
emonShimoul <emonshimoul@gmail.com>
2025-07-30 02:38:19 +0600

    pass by value

--- Module 15 (Pointer)/pointer.c
+++ Module 15 (Pointer)/pointer.c
@@ -3,12 +3,19 @@ int main()
 {
     int x=10;
     printf("%d\n",x);
-    printf("%p\n",&x);
+    // printf("%p\n",&x);
 
-    int* ptr;
+    int* ptr;  // declaring a pointer variable
 
-    ptr = &x;
-    printf("%p\n",ptr);
-    printf("%p\n",&ptr);
+    ptr = &x;  // set the address of a variable into pointer variable
+
+    // printf("%p\n",ptr);
+    // printf("%p\n",&ptr);
+    printf("%d\n",*ptr); // dereferencing a pointer
+
+    *ptr = 200; // dereferencing a pointer
+
+    printf("%d\n",x);
+
     return 0;
 }
